a7f6361374767a3215a58459f5ccc4f67693f2d4
Sam <samkitchen94@gmail.com>
2019-05-14 10:34:55 +0100

    using forms chapter completed

--- app.rb
+++ app.rb
@@ -1,19 +1,12 @@
 require 'sinatra'
 
-
 get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
 
-# get '/named-cat' do
-  # p params[:name]
-  # @name = params[:name]
-  # erb(:index)
-# end
-
 get '/named-cat' do
-  p params[:name]
-  @name = params[:name]
-  erb(:index)
-end
+    p params[:name]
+    @name = params[:name]
+    erb(:index)
+  end
